993ef82cbe8bdce225e64c86bbe46152b260ece8
Hiulaptop <tlthieu200806@gmail.com>
2025-12-04 14:52:06 +0700

    Fix route in main bug

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ from src.presentation.circle_routes import router as circle_router # Import circ
 from src.presentation.notification_routes import router as notification_router
 from src.presentation.admin_log_routes import router as admin_log_router
 from src.presentation.user_routes import router as user_router # Import user_router
+from src.presentation.ai_routes import router as ai_router # Import ai_router
+from src.presentation.trip_routes import router as trip_router
 from src.infrastructure.database.sql.database import create_db_and_tables # Import for DB creation
 import src.infrastructure # Import the infrastructure package to load all models
 from contextlib import asynccontextmanager
@@ -33,6 +35,9 @@ app.include_router(circle_router, prefix="/api", tags=["circles"]) # Include cir
 app.include_router(notification_router, prefix="/api", tags=["notifications"])
 app.include_router(admin_log_router, prefix="/api", tags=["admin_logs"])
 app.include_router(user_router, prefix="/api", tags=["users"]) # Include user_router
+app.include_router(ai_router, prefix="/api", tags=["ai"]) # Include ai_router
+app.include_router(trip_router, prefix="/api", tags=["trips"])
+
 # app.include_router(circles.router, prefix="/api", tags=["circles"])
 # app.include_router(circle_members.router, prefix="/api", tags=["circle_members"])
 
